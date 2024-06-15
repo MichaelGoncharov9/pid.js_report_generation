ef70d8c92932c1af6fa1f770a1b834feabfdd6db
MichaelGoncharov9 <goncharovmichael.o@gmail.com>
2024-06-15 15:51:01 +0000

    add reg filtr

--- teile/filtr.sh
+++ teile/filtr.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-read -p "Нужна ли фильтрация? (y/n) " filtr
-if [[ $filtr == 'y' || $filtr == 'Y' ]]; then
-    read -p "True/False? (t/f) " value
-    if [[ $value == 't' || $value == 'T' ]]; then
-        value="true"
-        cat report.js | grep -B 3 -A 1 "$value"
-    elif [[ $value == 'f' || $value == 'F' ]]; then
-        value="false"
-        cat report.js | grep -B 3 -A 1 "$value"
+read -p "Нужна ли фильтрация? (y/n) " choice
+if [[ $choice == 'y' || $choice == 'Y' ]]; then
+    read -p "Фильтрация по регулярному выражению? (y/n) " choice
+    if [[ $choice == 'y' || $choice == 'Y' ]]; then
+        read -p "Введите регулярное выражение: " value
+        export REG="$value"
+        cat report.js | grep -E "$REG"
+    else
+        read -p "true/false? (t/f) " value
+        if [[ $value == 't' || $value == 'T' ]]; then
+            value="true"
+            cat report.js | grep -B 3 -A 1 "$value"
+        elif [[ $value == 'f' || $value == 'F' ]]; then
+            value="false"
+            cat report.js | grep -B 3 -A 1 "$value"
+        fi
     fi
 else
     echo "Без фильтрации."
